4913b4b99340a10b1755c70709492f74d6dca921
Tom Rose <tomrose@umich.edu>
2024-11-21 13:20:12 -0500

    entrance basic

--- _Entrance.py
+++ _Entrance.py
@@ -1,5 +1,33 @@
 def Entrance():
     
-    directions = ["forward"]
+    directions = ["forward"]    
+    hasTorch = False
+    
+    print("YOU ARE IN THE ENTRANCE")   
+    torchChoice = input("TAKE TORCH? (y/n)")
+    
+    if torchChoice.upper() == "Y":        
+        hasTorch = True
+    
+    else:     
+        hasTorch = False
+        print("NO TORCH")
+    
+    print("YOU CAN TRAVEL ONLY FORWARD")
+    print("WHERE DO YOU WANT TO MOVE?")
+    userInput = ''
+    
+    while userInput not in directions:        
+        print("CHOICE(S): FORWARD")
+        userInput = input()
+
+        if userInput == "forward":   
+            print("FORWARD: TO ENCAMPMENT") #Test Print
+            
+            if hasTorch == True:
+                print("THE LIGHT OF THE TORCH GUIDES YOUR WAY")
+            
+        else:
+            print("Please enter a valid direction.")
     
     return
