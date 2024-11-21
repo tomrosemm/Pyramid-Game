92d920a9319c3059709e7dadc9812b4e6f8689ee
Tom Rose <tomrose@umich.edu>
2024-11-21 13:54:36 -0500

    snakeway initial

--- _SnakeWay.py
+++ _SnakeWay.py
@@ -2,4 +2,36 @@ def SnakeWay():
     
     directions = ["forward", "backward"]
     
-    return
+    hasTorch = True #Test variable, in execution will be linked to the action of taking torch in entrance
+    
+    print("YOU ARE IN THE SNAKEWAY")
+    
+    if hasTorch == False:
+        print("YOU DON'T HAVE A LIGHT SOURCE, AND IT'S PITCH BLACK. YOU HEAR HISSING")
+            
+    else:
+        print("YOU HAVE A HANDY DANDY TORCH")
+    
+    print("YOU CAN TRAVEL FORWARD OR BACKWARD")
+    print("WHERE DO YOU WANT TO MOVE?")
+    userInput = ''
+    
+    while userInput not in directions:
+        print("CHOICE(S): FORWARD, BACKWARD")
+        userInput = input()
+
+        if userInput == "forward":   
+            print("FORWARD: TO SPHINX ROOM") #Test Print
+            
+            if hasTorch == True:
+                print("THE LIGHT OF THE TORCH ALLOWS YOU TO COMPLETELY AVOID THE SNAKES")
+            
+            else:
+                print("YOU STUMBLE THROUGH THE DARKNESS AND INTO MORE THAN A COUPLE SNAKES. THEY BITE.")
+                #Take damage
+            
+            #SphinxRoom()
+            
+        else:
+            print("Please enter a valid direction.")
+    
