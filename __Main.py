57e97f07ace65b569a50cced8afd276a8a5a4f33
Tom Rose <tomrose@umich.edu>
2024-11-23 16:37:35 -0500

    objects for combat/else in main

--- __Main.py
+++ __Main.py
@@ -9,8 +9,25 @@ import _SandyCove
 #Royal Tomb
 #Treasure Chamber
 
-hasTorch = False
-hasAxe = False
-hasSword = False
-riddleAnswer = "THE ANSWER"
+#Import Systems
+import Combat
 
+#Import Libraries
+import random
+import time
+
+player = {
+    "name": "Player",
+    "health": 100,
+    "baseDamageRange": (10,20),
+    "hasAxe": False,
+    "hasSword": False,
+    "axeBonus": (5, 10),
+    "swordBonus": (10,15)
+}
+
+tombGoblin = {
+    "name": "Goblin Fred",
+    "health": 75,
+    "damageRange": (5,15)
+}
